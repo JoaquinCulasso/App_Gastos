61ed2db2619a7282c14b631f736229bf6f291f2e
Joaquin Culasso <joaquin.culasso@gmail.com>
2020-03-14 20:14:16 -0300

    Ejercitando flutter con firebase

---
+++ lib/Views/HomePage.dart
@@ -0,0 +1,230 @@
+import 'package:expenses/Controllers/graph_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  PageController _pageController;
+  int currentPage = 9;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Firestore.instance
+        .collection('expenses')
+        .where("month", isEqualTo: currentPage + 1)
+        .snapshots()
+        .listen((data) => data.documents.forEach((doc) => print(doc['category'])));
+
+    _pageController = PageController(
+      initialPage: currentPage,
+      viewportFraction: 0.4,
+    );
+  }
+
+  Widget _bottonAction(IconData icon) {
+    return InkWell(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Icon(icon),
+      ),
+      onTap: () {},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 8.0,
+        shape: CircularNotchedRectangle(),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _bottonAction(FontAwesomeIcons.history),
+            _bottonAction(FontAwesomeIcons.chartPie),
+            SizedBox(width: 48.0),
+            _bottonAction(FontAwesomeIcons.wallet),
+            _bottonAction(Icons.settings),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          _selector(),
+          _expenses(),
+          _graph(),
+          Container(
+            color: Colors.blueAccent.withOpacity(0.15),
+            height: 24.0,
+          ),
+          _list(),
+        ],
+      ),
+    );
+  }
+
+  Widget _pageItem(String name, int position) {
+    var _alignment;
+
+    final selected = TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.blueGrey,
+    );
+
+    final unselected = TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.blueGrey.withOpacity(0.4),
+    );
+
+    if (position == currentPage) {
+      _alignment = Alignment.center;
+    } else if (position > currentPage) {
+      _alignment = Alignment.centerRight;
+    } else {
+      _alignment = Alignment.centerLeft;
+    }
+
+    return Align(
+      alignment: _alignment,
+      child: Text(
+        name,
+        style: position == currentPage ? selected : unselected,
+      ),
+    );
+  }
+
+  Widget _selector() {
+    return SizedBox.fromSize(
+      size: Size.fromHeight(70.0),
+      child: PageView(
+        onPageChanged: (newPage) {
+          setState(() {
+            currentPage = newPage;
+          });
+        },
+        controller: _pageController,
+        children: <Widget>[
+          _pageItem("Enero", 0),
+          _pageItem("Febrero", 1),
+          _pageItem("Marzo", 2),
+          _pageItem("Abril", 3),
+          _pageItem("Mayo", 4),
+          _pageItem("Junio", 5),
+          _pageItem("Julio", 6),
+          _pageItem("Agosto", 7),
+          _pageItem("Septiembre", 8),
+          _pageItem("Octubre", 9),
+          _pageItem("Noviembre", 10),
+          _pageItem("Diciembre", 11),
+        ],
+      ),
+    );
+  }
+
+  Widget _expenses() {
+    return Column(
+      children: <Widget>[
+        Text(
+          "\$2361,41",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 40.0,
+          ),
+        ),
+        Text(
+          "Total de gastos",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16.0,
+            color: Colors.blueGrey,
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _graph() {
+    return Container(
+      height: 250.0,
+      child: GraphWidget(),
+    );
+  }
+
+  Widget _item(IconData icon, String name, int percent, double value) {
+    return ListTile(
+      leading: Icon(icon, size: 32.0),
+      title: Text(
+        name,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20.0,
+        ),
+      ),
+      subtitle: Text(
+        "$percent% of expenses",
+        style: TextStyle(
+          fontSize: 16.0,
+          color: Colors.blueGrey,
+        ),
+      ),
+      trailing: Container(
+        decoration: BoxDecoration(
+          color: Colors.blueAccent.withOpacity(0.2),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            "\$$value",
+            style: TextStyle(
+              color: Colors.blueAccent,
+              fontWeight: FontWeight.bold,
+              fontSize: 15.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _list() {
+    return Expanded(
+      child: ListView.separated(
+        itemCount: 15,
+        itemBuilder: (BuildContext context, int index) => _item(
+          FontAwesomeIcons.shoppingCart,
+          "Shopping",
+          14,
+          145.12,
+        ),
+        separatorBuilder: (BuildContext context, int index) {
+          return Container(
+            color: Colors.blueAccent.withOpacity(0.15),
+            height: 8.0,
+          );
+        },
+      ),
+    );
+  }
+}
